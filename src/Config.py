946b0c6954bbf6d9bf3e8032a47b0b908756ceca
KarolKul-KK <kar.kulinski@gmail.com>
2023-04-17 10:49:22 +0200

    Added module to load configs

---
+++ src/Config.py
@@ -0,0 +1,14 @@
+import yaml
+
+
+class ConfigLoader:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.config = self.load_config()
+
+    def load_config(self):
+        with open(self.config_file, 'r') as f:
+            return yaml.safe_load(f)
+
+    def get_config(self):
+        return self.config
