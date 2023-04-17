adc4192564afba48b3f0cd152430aa36e37abd7d
KarolKul-KK <kar.kulinski@gmail.com>
2023-04-17 17:11:14 +0200

    Added python module for manage config files

--- src/Config.py
+++ src/Config.py
@@ -1,14 +1,41 @@
 import yaml
+from pathlib import Path
 
 
 class ConfigLoader:
-    def __init__(self, config_file):
-        self.config_file = config_file
+    def __init__(self, base_path, config_file):
+        self.base_path = Path(base_path)
+        self.config_file = self.base_path / config_file
         self.config = self.load_config()
 
-    def load_config(self):
-        with open(self.config_file, 'r') as f:
+    def load_config(self, path=None):
+        if path:
+            config_path = path
+        else:
+            config_path = self.config_file
+        with open(config_path, 'r') as f:
             return yaml.safe_load(f)
 
     def get_config(self):
         return self.config
+    
+    def get_source_keys(self):
+        return list(self.config['sources'].keys())
+    
+    def get_config_path(self, *args):
+        path = Path(self.base_path)
+        for arg in args:
+            if arg:
+                path = path / arg
+        return str(path)
+    
+    def config_generator(self):
+        keys = self.get_source_keys()
+        for key in keys:
+            credenstial_path = self.get_config_path(key, 'credenstials.yml')
+            credenstial = self.load_config(credenstial_path)
+            tables = self.get_config()['sources'][key]
+            for table in tables:
+                table_path = self.get_config_path(key, 'tables', table)
+                table_info = self.load_config(table_path)
+                yield key, credenstial, table_info
