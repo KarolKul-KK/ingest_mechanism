a945265b0bb86b932156a0e81f05093dd921b8e7
KarolKul-KK <kar.kulinski@gmail.com>
2023-04-22 16:41:39 +0200

    Update PyMySQL class to handle string bug inside pymysql connection object and added _clean_query function to handle white spaces

--- src/MySql_connector.py
+++ src/MySql_connector.py
@@ -1,9 +1,15 @@
 import pymysql
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
 from typing import Dict, List, Tuple, Any
 
 
 class PyMySQL:
     def __init__(self, host: str, user: str, password: str, database: str):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
         self.connection = pymysql.connect(
             host=host,
             user=user,
@@ -11,8 +17,12 @@ class PyMySQL:
             database=database
         )
         self.cursor = self.connection.cursor()
+        
+    def _clean_query(self, query: str) -> str:
+        return query.strip()
 
     def execute_query(self, query: str) -> List[Tuple[Any]]:
+        query = self._clean_query(query)
         self.cursor.execute(query)
         return self.cursor.fetchall()
 
@@ -22,6 +32,20 @@ class PyMySQL:
         query = f"INSERT INTO {table} ({columns}) VALUES ({placeholders})"
         self.cursor.execute(query, tuple(data.values()))
         self.connection.commit()
+
+    def read_data_to_spark(self, spark, table: str, query: str = None):
+        url = f"jdbc:mysql://{self.host}/{self.database}"
+        properties = {
+            "user": self.user,
+            "password": self.password,
+            "driver": "com.mysql.jdbc.Driver"
+        }
+        if query:
+            query = self._clean_query(query)
+            df = spark.read.jdbc(url=url, table=f"({query}) as tmp", properties=properties)
+        else:
+            df = spark.read.jdbc(url=url, table=table, properties=properties)
+        return df
     
     def close_connection(self) -> None:
         self.cursor.close()
