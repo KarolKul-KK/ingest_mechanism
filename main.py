ecbec1fe3420b04d03ea975b54d1a8cef4414cbb
KarolKul-KK <kar.kulinski@gmail.com>
2023-04-29 20:07:17 +0200

    Add support for SQL case, creation of Spark session and Spark transformation for SQL tables

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from src.Config import ConfigLoader
 from src.MySql_connector import PyMySQL
 from src.Riot_api import RiotAPI, DataDragonAPI
+from src.Spark_utils import SparkUtils, convert_to_int_udf
+from src.Delta_module import DeltaModule
 
 
-def run_pipeline(generator):
+def run_pipeline(generator, spark):
     while True:
         try:
-            key, credentials, tables_info = next(generator)
+            key, credenstials, tables_info = next(generator)
+            pymysql = PyMySQL(credenstials['host'], credenstials['username'], credenstials['password'], credenstials['database'])
             if key == 'sql':
-                # handle sql case
-                pass
+                df = pymysql.read_data_to_spark(spark, tables_info['name'], tables_info['query'])
+            if tables_info['name'] == 'general_stats':
+                df = SparkUtils.extract_date_event(df, 'Date')
+            if tables_info['name'] == 'team_stats':
+                df = SparkUtils.convert_column_to_bool(df, 'First_Blood')
+                df = SparkUtils.convert_column_to_bool(df, 'First_Tower')
+                df = df.withColumn("Gold", convert_to_int_udf(df["Gold"]))
+            if tables_info['name'] == 'player_stats':
+                df = SparkUtils.extract_kda(df, 'KDA')
             elif key == 'api':
                 # handle api case
                 pass
@@ -25,6 +35,7 @@ def run_pipeline(generator):
             print(f"Error: {e}")
 
 if __name__ == '__main__':
+    spark = SparkUtils.create_spark_session('ingest_mechanism', '/home/jovyan/mysql-connector-java-8.0.27')
     config = ConfigLoader('configs/', 'job_config.yml')
     generator = config.config_generator()
-    run_pipeline(generator)
+    run_pipeline(generator, spark)
